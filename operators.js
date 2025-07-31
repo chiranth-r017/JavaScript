7c24b6513c96a0b2e8e747f48e476a80b75d92ad
chiranth-r017 <chiranth.parvam@gmail.com>
2025-07-31 10:26:20 +0530

    JavaScript

---
+++ operators.js
@@ -0,0 +1,24 @@
+num1 = 50;
+num2 = 80;
+num3 = "50";
+
+// Arithmetic Operators
+console.log("Addition:",num1 + num2);
+console.log("Subtraction:",num2 - num1);
+console.log("Multiplication:",num1 * num2);
+console.log("Division for Quotient:",num1 / 25);
+console.log("Division for Remainder:",num1 % 25);
+console.log("Power:",num1 ** 3);
+
+// Relational Operators
+console.log("Greater: ", num1 > num2);
+console.log("Greater: ", num2 > num1);
+console.log("Lesser: ", num1 < num2);
+console.log("Lesser: ", num2 < num1);
+console.log("Greater or Equal to: ", num1 >= num2);
+console.log("Greater or Equal to: ", num2 >= num1);
+console.log("Lesser or Equal to: ", num1 <= num2);
+console.log("Lesser or Equal to: ", num2 <= num1);
+console.log("Equal to: ", num1 == num3); // Check only value // Equal to
+console.log("Equal to: ", num1 === num3); // Check value along with datatype // Strictly Equal to
+
---
+++ script.js
@@ -0,0 +1,33 @@
+// Javascript is a Programming Language, Scripting Language & also Dynamically Typed Language(no need to define the datatype)
+// JavaScript is a high-level, often just-in-time compiled, and multi-paradigm programming language primarily used to make web pages interactive. It's a core technology of the World Wide Web, enabling dynamic content, interactive user interfaces, and other features not possible with just HTML and CSS. Beyond the browser, JavaScript is also used for server-side development, game development, and mobile app creation. 
+// Key aspects of JavaScript:
+// Client-side scripting:
+// JavaScript is most well-known for its role in adding interactivity to web pages. It allows developers to respond to user actions, manipulate web page content, and create dynamic elements like animations and slideshows. 
+// Dynamic content:
+// JavaScript can update and change HTML and CSS, allowing for real-time updates, form validation, and more complex interactions on a webpage. 
+// Cross-platform:
+// JavaScript is a lightweight language that can run in various environments, making it suitable for both front-end and back-end development. 
+// Multi-paradigm:
+// JavaScript supports multiple programming paradigms, including object-oriented, imperative, and functional programming. 
+// Beyond the browser:
+// While its primary use is for web development, JavaScript is also used in other contexts like server-side development (Node.js), game development, and mobile app development. 
+// Core technologies of the web:
+// It's one of the three fundamental languages for web development, along with HTML and CSS. 
+console.log("Hello World from Script.js");
+var person_name = "Akshay";
+var person_age = 24;
+var height = 105.8;
+var isAdult = true;
+var company = null;
+// Syntax: var variable_name = "Value";
+// var is a keyword used to declare the variables
+console.log("Names of the person:", person_name);
+console.log(typeof(person_name)); // string datatype
+console.log("Age of the person:", person_age);
+console.log(typeof(person_age)); // number datatype
+console.log("Height of the person:", height);
+console.log(typeof(height)); // number datatype
+console.log("Adult/Child:", isAdult);
+console.log(typeof(isAdult));
+console.log(typeof(gender)); // undefined refers to the variable which has not be declared
+console.log(typeof(company)); 
